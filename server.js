ab1f252575701d1bb07b46917d205746ad0e6da0
piblo <paulhankins99@gmail.com>
2020-08-08 19:59:10 -0500

    connected database and fixed deprecation warnings

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+// connect to our Mongo DB:
+// use object containing { useNewUrlParser: true, useUnifiedTopology: true}
+// as a parameter to remove deprecation warning
+mongoose.connect('mongodb://localhost/world', { useNewUrlParser: true, useUnifiedTopology: true}, )
+.then(data => {
+    console.log('Mongo DB conncetion success!')
+})
+.catch(err => {
+    console.log('Mongo DB conncetion failed: ' + err.message)
+})
+
 const app = express()
 
 app.use('/', (req, res, next) => {
